09d9c3d31ba3b37d6417fa6d35d9db034f941631
alitariq01 <84877800+alitariq01@users.noreply.github.com>
2021-05-26 23:28:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from instadm import instaDM
 if __name__ == '__main__':
-         insta = InstaDM(username='your_username', password='your_password', headless=False)
+        insta = InstaDM(username='your_username', password='your_password', headless=False)
 insta.send.Message(user='username_target', message='hi')
 insta.sendGroupMessage(users=['user1', 'user2'] message='hi')
